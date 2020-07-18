318a4a38022df17bd34c5b712870b5d3ece1d89e
Kittipong <68457461+Kittipong6306021630013@users.noreply.github.com>
2020-07-18 13:57:42 +0700

    Add files via upload

---
+++ grade.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{	
+	cout<<"   #  "<<endl;
+	cout<<"  # #  "<<endl;
+	cout<<" #   #  "<<endl;
+	cout<<"#     #"<<endl;
+	cout<<"####### "<<endl;
+	cout<<"#     #"<<endl;
+	cout<<"Kittipong Pounglaya"<<endl;
+	cin.get();
+	return 0;
